140d9b4762873692bd4c90e43b9cdaef6f68c235
Dominik277 <dominikdoric277@gmail.com>
2020-10-29 18:11:13 +0100

    Sucelje sa username i password.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View view){
+    public void login(View view){
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
         Log.i("Info","Button pressed");
+        Log.i("Username", usernameEditText.getText().toString());
+        Log.i("Username", passwordEditText.getText().toString());
     }
 
     @Override
