4e85af2e419fb1835fe50d7f5dc27aeb48e73171
Dominik277 <dominikdoric277@gmail.com>
2020-10-30 10:41:30 +0100

    Higher or lower game.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -5,25 +5,41 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void convertCurrency(View view){
-        Log.i("Info","Button pressed");
+    int randomNumber;
+    public void generateRandomNumber(){
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;
+    }
+
+    public void guess (View view){
         EditText editText = (EditText) findViewById(R.id.editText);
-        String amountInPounds = editText.getText().toString();
-        double amountInPoundsDouble = Double.parseDouble(amountInPounds);
-        double amountInDollarsDouble = amountInPoundsDouble * 1.3;
-        String amountInDollarsString = String.format("%.2f",amountInDollarsDouble);
-        Toast.makeText(this, "&" + amountInPounds + "is $ " + amountInDollarsString,
-                Toast.LENGTH_LONG).show();
+        int guessValue = Integer.parseInt(editText.getText().toString());
+        String message;
+        if(guessValue > randomNumber){
+            message = "Lower!";
+        }else if (guessValue < randomNumber){
+            message = "Higher!";
+        }else {
+            message = "You got it right!Try again!";
+            generateRandomNumber();
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        Log.i("Entered value",editText.getText().toString());
+        Log.i("Random number",Integer.toString(randomNumber));
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        generateRandomNumber();
     }
 }
