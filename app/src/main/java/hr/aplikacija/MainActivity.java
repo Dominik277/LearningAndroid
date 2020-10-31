e1d9e7d8d470e4285d9a13bdd9dea7d69cdbe2ee
Dominik277 <dominikdoric277@gmail.com>
2020-10-31 12:23:19 +0100

    ListView.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -5,20 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void playPhrase(View view){
-        Button buttonPressed = (Button) view;
-        MediaPlayer mediaPlayer = MediaPlayer.create(this,getResources()
-                .getIdentifier(buttonPressed.getTag().toString(), "raw", getPackageName()));
-        mediaPlayer.start();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily = new ArrayList<String>();
+        myFamily.add("Jack");
+        myFamily.add("Jane");
+        myFamily.add("Bill");
+        myFamily.add("Betty");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1,myFamily);
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
     }
 }
