41df2d978e30ac2ae315403b0590178071204772
Dominik277 <dominikdoric277@gmail.com>
2020-10-31 21:38:47 +0100

    ListView mala aplikaija sa Toast-om.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,16 +1,15 @@
 package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,24 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView myListView = findViewById(R.id.myListView);
-
-        ArrayList<String> myFamily = new ArrayList<String>();
-        myFamily.add("Jack");
-        myFamily.add("Jane");
-        myFamily.add("Bill");
-        myFamily.add("Betty");
-
+        ListView friednsListView = findViewById(R.id.friendListView);
+        final ArrayList<String> myFriends = new ArrayList<String>(asList("Mark","Jane","Sussy","Jan"));
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1,myFamily);
-        myListView.setAdapter(arrayAdapter);
-
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                (this, android.R.layout.simple_list_item_1, myFriends);
+        friednsListView.setAdapter(arrayAdapter);
+        friednsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),
+                        "Hello " + myFriends.get(i), Toast.LENGTH_LONG).show();
             }
         });
-
     }
 }
