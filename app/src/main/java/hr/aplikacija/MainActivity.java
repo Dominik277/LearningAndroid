f0f7407ad03bcb07dca41163372c96b0d7286fa8
Dominik277 <dominikdoric277@gmail.com>
2020-10-31 22:38:19 +0100

    Times table app.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -2,16 +2,25 @@ package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
+import android.widget.SeekBar;
 import java.util.ArrayList;
-import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView timesTablesListView;
+    public void generateTimesTable(int timesTableNumber){
+        Log.i("Seekbar Value",Integer.toString(timesTableNumber));
+        ArrayList<String> timesTableContent = new ArrayList<String>();
+        for(int j= 1; j <= 10; j++){
+            timesTableContent.add(Integer.toString(j * timesTableNumber));
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, timesTableContent);
+        timesTablesListView.setAdapter(arrayAdapter);
+    }
 
 
     @Override
@@ -19,16 +28,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView friednsListView = findViewById(R.id.friendListView);
-        final ArrayList<String> myFriends = new ArrayList<String>(asList("Mark","Jane","Sussy","Jan"));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1, myFriends);
-        friednsListView.setAdapter(arrayAdapter);
-        friednsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final SeekBar timesTablesSeekBar = findViewById(R.id.timesTablesSeekBar);
+        timesTablesListView =  findViewById(R.id.timesTablesTablesListView);
+
+        int max=20;
+        int startingPosition=5;
+        timesTablesSeekBar.setMax(max);
+        timesTablesSeekBar.setProgress(startingPosition);
+        generateTimesTable(10);
+
+        timesTablesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),
-                        "Hello " + myFriends.get(i), Toast.LENGTH_LONG).show();
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int timesTableNumber;
+                if(i < min){
+                    timesTableNumber = min;
+                    timesTablesSeekBar.setProgress(min);
+                }else {
+                    timesTableNumber = i;
+                }
+                Log.i("Seekbar Value",Integer.toString(timesTableNumber));
+                generateTimesTable(timesTableNumber);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
