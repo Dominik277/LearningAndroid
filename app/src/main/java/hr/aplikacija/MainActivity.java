ef2b98af6048d2fec54261fe616c3318f443916b
Dominik277 <dominikdoric277@gmail.com>
2020-11-01 09:58:32 +0100

    Egg timer app zavrsena.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,32 +1,56 @@
 package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView timerTextView;
     SeekBar timerSeekBar;
+    Boolean counterIsActive = false;
+    Button goButton;
+    CountDownTimer countDownTimer;
 
-    public void buttonClicked(View view){
-        new CountDownTimer(timerSeekBar.getProgress()*1000,1000){
-
-            @Override
-            public void onTick(long l) {
-                updateTimer((int)l/1000);
-            }
-
-            @Override
-            public void onFinish() {
-                Log.i("Finished!","Timer all done!");
-            }
-        }.start();
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        goButton.setText("GO!");
+        counterIsActive = false;
     }
 
+    public void buttonClicked(View view) {
+        if (counterIsActive) {
+              resetTimer();
+        } else {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            goButton.setText("STOP!");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long l) {
+                    updateTimer((int) l / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mplayer.start();
+                    resetTimer();
+                }
+            }.start();
+        }
+    }
     public void updateTimer(int secondsLeft){
         int minutes = secondsLeft / 60;
         int seconds = secondsLeft - (minutes * 60);
@@ -46,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
         timerSeekBar = findViewById(R.id.timerSeekBar);
         timerTextView = findViewById(R.id.countdownTextView);
+        goButton = findViewById(R.id.goButton);
 
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
