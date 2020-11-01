71f7ed9854e06590e1b59c859cd9b057a0f76fc3
Dominik277 <dominikdoric277@gmail.com>
2020-11-01 16:28:27 +0100

    Downloading web sadrzaj.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,96 +1,44 @@
 package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Random;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button goButton;
-    ArrayList<Integer> answers = new ArrayList<Integer>();
-    int locationOfCorrectAnswer;
-    TextView resultTextView;
-    int score = 0;
-    int numberOfQuestions = 0;
-    TextView scoreTextView;
-    Button button0;
-    Button button1;
-    Button button2;
-    Button button3;
-    TextView sumTextView;
-    TextView timerTextView;
-    Button playAgainButton;
+    public class DownloadTask extends AsyncTask<String,Void,String> {
 
-    public void playAgain(View view){
-        score = 0;
-        numberOfQuestions = 0;
-        timerTextView.setText("30s");
-        scoreTextView.setText(Integer.toString(score)
-                + "/" + Integer.toString(numberOfQuestions));
-        newQuestion();
-        playAgainButton.setVisibility(View.INVISIBLE);
-        new CountDownTimer(5100,100){
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpsURLConnection urlConnection = null;
 
-            @Override
-            public void onTick(long l) {
-                timerTextView.setText(String.valueOf(l/1000) + "s");
-            }
-
-            @Override
-            public void onFinish() {
-                resultTextView.setText("Zavrseno!");
-                playAgainButton.setVisibility(View.VISIBLE);
-            }
-        }.start();
-    }
-
-    public void chooseAnswer(View view){
-        if (Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())){
-            resultTextView.setText("Tocno!");
-            score++;
-        }else {
-            resultTextView.setText("Netocno!");
-        }
-            numberOfQuestions++;
-            scoreTextView.setText(Integer.toString(score)
-                    + "/" + Integer.toString(numberOfQuestions));
-            newQuestion();
-    }
-
-    public void start(View view){
-        goButton.setVisibility(View.INVISIBLE);
-    }
-
-    public void newQuestion(){
-        Random rand = new Random();
-        int a = rand.nextInt(21);
-        int b = rand.nextInt(21);
-        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
-
-        locationOfCorrectAnswer = rand.nextInt(4);
-        answers.clear();
-
-        for (int i=0; i<4 ;i++){
-            if(i==locationOfCorrectAnswer) {
-                answers.add(a+b);
-            }else {
-                int wrongAnswer = rand.nextInt(41);
-                while (wrongAnswer == a+b){
-                    wrongAnswer = rand.nextInt(41);
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpsURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while (data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
                 }
-                answers.add(wrongAnswer);
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed!";
             }
         }
-        button0.setText(Integer.toString(answers.get(0)));
-        button1.setText(Integer.toString(answers.get(1)));
-        button2.setText(Integer.toString(answers.get(2)));
-        button3.setText(Integer.toString(answers.get(3)));
     }
 
     @Override
@@ -98,19 +46,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sumTextView = findViewById(R.id.sumTextView);
-        button0 = findViewById(R.id.button0);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        resultTextView = findViewById(R.id.resultTextView);
-        scoreTextView = findViewById(R.id.scoreTextView);
-        goButton = findViewById(R.id.goButton);
-        timerTextView = findViewById(R.id.timerTextView);
-        playAgainButton = findViewById(R.id.playAgainButton);
-
-        newQuestion();
-
-        playAgain(findViewById(R.id.goButton));
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("https://zappycode.com").get();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Log.i("Result!",result);
     }
 }
