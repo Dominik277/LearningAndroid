843de257d58d26cfe4cd9838e7a1b915bc4a094c
Dominik277 <dominikdoric277@gmail.com>
2020-11-01 17:30:39 +0100

    Skidanje slike s web-a.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -2,43 +2,32 @@ package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    public class DownloadTask extends AsyncTask<String,Void,String> {
-
-        @Override
-        protected String doInBackground(String... urls) {
-            String result = "";
-            URL url;
-            HttpsURLConnection urlConnection = null;
-
-            try {
-                url = new URL(urls[0]);
-                urlConnection = (HttpsURLConnection) url.openConnection();
-                InputStream in = urlConnection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(in);
-                int data = reader.read();
-                while (data != -1){
-                    char current = (char) data;
-                    result += current;
-                    data = reader.read();
-                }
-                return result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "Failed!";
-            }
+    ImageView imageView;
+    public void downloadImage(View view){
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png").get();
+            imageView.setImageBitmap(myImage);
+        }catch (Exception e){
+            e.printStackTrace();
         }
+
     }
 
     @Override
@@ -46,13 +35,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadTask task = new DownloadTask();
-        String result = null;
-        try {
-            result = task.execute("https://zappycode.com").get();
-        }catch (Exception e){
-            e.printStackTrace();
+        imageView = findViewById(R.id.imageView);
+    }
+    public class ImageDownloader extends AsyncTask<String,Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls{0})
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(in);
+                return myBitmap;
+            }catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
         }
-        Log.i("Result!",result);
     }
 }
