9b0b74e3095b87acfad1028346fb8c42c081c91c
Dominik277 <dominikdoric277@gmail.com>
2020-11-01 20:34:33 +0100

    Webview.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,11 +1,9 @@
 package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,29 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Events",MODE_PRIVATE,null);
-            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS theNewUsers (name VARCHAR, age INT(4), id INTEGER PRIMARY KEY)");
-            sqLiteDatabase.execSQL("INSERT INTO theNewUsers (name,age) VALUES ('Tom',21)");
-            sqLiteDatabase.execSQL("INSERT INTO theNewUsers (name,age) VALUES ('Nick',47)");
-            sqLiteDatabase.execSQL("INSERT INTO theNewUsers (name,age) VALUES ('Nick',37)");
-
-            Cursor c = sqLiteDatabase.rawQuery("SELECT * FROM newUsers",null);
-            int nameIndex = c.getColumnIndex("name");
-            int ageIndex = c.getColumnIndex("age");
-            int idIndex = c.getColumnIndex("id");
-            c.moveToFirst();
-
-            while (c != null){
-                Log.i("UserResults-event",c.getString(nameIndex));
-                Log.i("UserResults-age",Integer.toString(c.getInt(ageIndex)));
-                Log.i("userResults-id",Integer.toString(c.getInt(idIndex)));
-            }
-
-            c.moveToNext();
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        //webView.loadUrl("http://www.google.com");
+        webView.loadData("<html><body></body><h1>Hello world</h1></html>"
+                ,"text/html","UTF-8");
     }
 }
