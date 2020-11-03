082d97e2987374a44094ea0eb5d420d20804e1f3
Dominik277 <dominikdoric277@gmail.com>
2020-11-03 18:02:50 +0100

    Alert Dialogs.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,88 +1,30 @@
 package hr.aplikacija;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ShareCompat;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import java.util.ArrayList;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    ListView listView;
-    TextView statusTextView;
-    Button searchButton;
-    BluetoothAdapter bluetoothAdapter;
-    ArrayList<String> bluetootDevices = new ArrayList<>();
-    ArrayList<String> adresses = new ArrayList<>();
-    ArrayAdapter arrayAdapter;
-
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-          String action = intent.getAction();
-          Log.i("Action",action);
-          if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-              statusTextView.setText("Finished!");
-              searchButton.setEnabled(true);
-          }else if (BluetoothDevice.ACTION_FOUND.equals(action)){
-              BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-              String name = device.getName();
-              String adress = device.getAddress();
-              String rssi = Integer.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE));
-              //Log.i("Device Found!","Name: " + name + " Adress: " + adress + " RSSI: " + rssi);
-              if (!adresses.contains(adress)){
-                  adresses.add(adress);
-                  String deviceString = "";
-                  if(name == null || name.equals("")){
-                      deviceString = adress + " -RSSI " + rssi + "dBm";
-                  }else {
-                      deviceString = name + " -RSSI " + rssi + "dBm";
-                  }
-                  bluetootDevices.add(deviceString);
-                  arrayAdapter.notifyDataSetChanged();
-              }
-          }
-        }
-    };
-
-    public void searchClicked(View view){
-        statusTextView.setText("Searching...");
-        searchButton.setEnabled(false);
-        bluetootDevices.clear();
-        adresses.clear();
-        bluetoothAdapter.startDiscovery();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        statusTextView = findViewById(R.id.statusTextView);
-        searchButton = findViewById(R.id.searchButton);
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,bluetootDevices);
-        listView.setAdapter(arrayAdapter);
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
-        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        registerReceiver(broadcastReceiver,intentFilter);
-
-
-
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do you defenetly want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        Toast.makeText(MainActivity.this,"It's done!",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No!",null)
+                .show();
     }
 }
