82b095d1adbace88df19b3f988437ae7efbb0453
Dominik277 <dominikdoric277@gmail.com>
2020-11-04 09:43:27 +0100

    Uređivanje Action Bar-a.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,47 +1,36 @@
 package hr.aplikacija;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    SharedPreferences sharedPreferences;
-    TextView textView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu,menu);
+        menuInflater.inflate(R.menu.main_manu,menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
         switch (item.getItemId()){
-            case R.id.english:
-                setLanguage("English");
+            case R.id.settings:
+                Log.i("Item selected","Settings");
                 return true;
-            case R.id.spanish:
-                setLanguage("Spanish");
+            case R.id.help:
+                Log.i("Item selected","Help");
                 return true;
             default:
                 return false;
         }
-    }
-
-    public void setLanguage(String language){
-        sharedPreferences.edit().putString("language",language).apply();
-        textView.setText(language);
     }
 
     @Override
@@ -49,31 +38,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
-        sharedPreferences = this.getSharedPreferences("hr.aplikacija", Context.MODE_PRIVATE);
-        String languge = sharedPreferences.getString("language", "Error");
-        if (languge.equals("Error")) {
-            new AlertDialog.Builder(this)
-                    .setIcon(android.R.drawable.ic_btn_speak_now)
-                    .setTitle("Choose a language")
-                    .setMessage("What language would you like to use?")
-                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            //SET ENGLISH
-                            setLanguage("English");
-                        }
-                    })
-                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //Set spanish
-                            setLanguage("Spanih");
-                        }
-                    })
-                    .show();
-        }else {
-            textView.setText(languge);
-        }
     }
 }
