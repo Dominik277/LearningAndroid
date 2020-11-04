cda9cd5f0f3811c4599ad3a5afa87b98ec360c85
Dominik277 <dominikdoric277@gmail.com>
2020-11-04 10:35:24 +0100

    Konfiguracija View-a i spajanje sa listView-om.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,33 +1,25 @@
 package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences
-                ("hr.aplikacija", Context.MODE_PRIVATE);
-
-        ArrayList<String> friends = new ArrayList<>();
-        friends.add("Fido");
-        friends.add("Sarah");
-        friends.add("Jones");
-        sharedPreferences.edit().putString()
-
-        //sharedPreferences.edit().putString("username","nick").apply();
-        //String username = sharedPreferences.getString("username","");
-        //Log.i("This is username!",username);
+        ListView listView = findViewById(R.id.listView);
+        notes.add("Example Note");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
 
     }
 }
