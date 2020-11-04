b83ad0a1751494673571c12c44394914962455b0
Dominik277 <dominikdoric277@gmail.com>
2020-11-04 15:13:56 +0100

    Zavrsena Notes aplikacija.

--- app/src/main/java/hr/aplikacija/NoteEditorActivity.java
+++ app/src/main/java/hr/aplikacija/NoteEditorActivity.java
@@ -1,12 +1,17 @@
 package hr.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuInflater;
 import android.widget.EditText;
+
+import java.util.HashSet;
 
 public class NoteEditorActivity extends AppCompatActivity {
 
@@ -37,6 +42,10 @@ public class NoteEditorActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 MainActivity.notes.set(noteId, String.valueOf(charSequence));
                 MainActivity.arrayAdapter.notifyDataSetChanged();
+                SharedPreferences sharedPreferences = getApplicationContext()
+                        .getSharedPreferences("hr.aplikacija", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes",set).apply();
             }
             @Override
             public void afterTextChanged(Editable editable) {
