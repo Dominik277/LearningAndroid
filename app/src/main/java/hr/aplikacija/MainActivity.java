600154a991bba69cce3af07615935b672f6e6200
Dominik277 <dominikdoric277@gmail.com>
2020-11-07 20:03:38 +0100

    Dovrsen View.

--- app/src/main/java/hr/aplikacija/MainActivity.java
+++ app/src/main/java/hr/aplikacija/MainActivity.java
@@ -1,14 +1,92 @@
 package hr.aplikacija;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                updateLocationInfo(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String s) {
+
+            }
+        };
+        if (ContextCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }else {
+            locationManager.requestLocationUpdates
+                    (LocationManager.GPS_PROVIDER,0,0,locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastKnownLocation != null){
+                updateLocationInfo(lastKnownLocation);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+            }
+        }
+
+
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission
+                (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates
+                    (LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public void updateLocationInfo(Location location){
+        TextView latTextView = findViewById(R.id.latTextView);
+        TextView lonTextView = findViewById(R.id.lonTextView);
+        TextView accTextView = findViewById(R.id.accTextView);
+        TextView altTextView = findViewById(R.id.altTextView);
+        TextView adressTextView = findViewById(R.id.adressTextView);
+
+        latTextView.setText("Latitude: " + Double.toString(location.getLatitude()));
+        lonTextView.setText("Latitude: " + Double.toString(location.getLongitude()));
+        accTextView.setText("Latitude: " + Double.toString(location.getAccuracy()));
+        altTextView.setText("Latitude: " + Double.toString(location.getAltitude()));
     }
 }
